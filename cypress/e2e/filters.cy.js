f64eedc35cc652354c0a5670dbca8e4397233c65
usama madni <usama.madni-int@dubizzlelabs.com>
2025-07-01 14:57:58 +0500

    new code added

--- cypress/e2e/filters.cy.js
+++ cypress/e2e/filters.cy.js
@@ -1,37 +1,22 @@
-import Filters from "../page_objects/filters";
-import AddClient from "../page_objects/add_client";
+//imports
+
+
+//variables
 const email = "admin@jarvisempg.com";
 const password = "MASTER";
 
-const cname= "usama test"
-const desig = "Marketing Manager"
-const bname = "osama business"
-const ctype = 'Dealership - Sales Agent'
-const clfc = "Moderate"
-const area = "Abu Dhabi"
-const subarea = "Al Bidya"
+//login
+beforeEach(() => {
+  cy.login(email, password);
+});
 
-  beforeEach(() => {
-    cy.login(email, password);
+//test_cases
+describe("Filters Testing", () => {
+
+  it("Search by Business Name", () => {
+    
+
+
   });
 
-//   it("Client Filters", () => {
-//     const fl = new Filters();
-//     fl.refFilter(5)
-//   });
-// });
-
-describe("Create a client", () => {
-
-
-  it("with mandatory fields", () => {
-    const client = new AddClient();
-    client.addClient(555677654);
-    client.addName(cname)
-    client.designation(desig)
-    client.business(bname)
-    client.clientType(ctype)
-    client.classification(clfc)
-    client.location(area, subarea)
-  });
 });
--- cypress/page_objects/filters.js
+++ cypress/page_objects/filters.js
@@ -1,16 +1,49 @@
 class Filters {
-  refFilter(ref) {
-    cy.visit("https://prelivev2-vas-uae-affiliates.jarvisempg.com/admin/crm/clients");
+  //ref
+  refFilter(val) {
+    cy.visit(
+      "https://prelivev2-vas-uae-affiliates.jarvisempg.com/admin/crm/clients"
+    );
     cy.wait(3000);
-    cy.get('[placeholder="Search by Ref #"]').type(ref);
+    cy.get('[placeholder="Search by Ref #"]').type(val);
     cy.contains("button", "Search").click();
     cy.get("tbody")
       .should("be.visible")
       .within(() => {
         cy.get("td").then((cells) => {
-          const found = [...cells].some((cell) =>
-            cell.innerText.includes(ref)
-          )
+          const found = [...cells].some((cell) => cell.innerText.includes(val));
+        });
+      });
+  }
+  //contact
+  contact(val) {
+    cy.visit(
+      "https://prelivev2-vas-uae-affiliates.jarvisempg.com/admin/crm/clients"
+    );
+    cy.wait(3000);
+    cy.get('[placeholder="Search by Contact No."]').type(val);
+    cy.contains("button", "Search").click();
+    cy.get("tbody")
+      .should("be.visible")
+      .within(() => {
+        cy.get("td").then((cells) => {
+          const found = [...cells].some((cell) => cell.innerText.includes(val));
+        });
+      });
+  }
+  //email
+  email(val) {
+    cy.visit(
+      "https://prelivev2-vas-uae-affiliates.jarvisempg.com/admin/crm/clients"
+    );
+    cy.wait(3000);
+    cy.get('[placeholder="Search by Email""]').type(val);
+    cy.contains("button", "Search").click();
+    cy.get("tbody")
+      .should("be.visible")
+      .within(() => {
+        cy.get("td").then((cells) => {
+          const found = [...cells].some((cell) => cell.innerText.includes(val));
         });
       });
   }
